5520658c4ce52146dcbc7901898453975cb1db9e
elijuggz <alimansouri.dev@gmail.com>
2024-10-13 21:49:21 -0400

    tailwindcss

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
-  res.json({ fruits: ["apple", "fruit", "banana", "ayooo"] });
+  res.json({ fruits: ["test1", "test2", "test3", "test4", "test5"] });
 });
 
 app.listen(8080, () => {
